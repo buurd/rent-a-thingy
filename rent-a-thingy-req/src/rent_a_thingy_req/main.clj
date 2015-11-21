e8dccc45fbbdf8b80aed56a12ca61769dc9c4a6c
Roland Heimdahl <roland@javalia.se>
2015-11-21 22:36:51 +0100

    Fixes #3, lagt till requirements-projekt för att validera hela systemet rent-a-thingy

---
+++ rent-a-thingy-req/src/rent_a_thingy_req/main.clj
@@ -0,0 +1,18 @@
+(ns rent-a-thingy-req.main
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [clj-http.client :as client]
+            [rent-a-thingy-req.handler :refer :all]))
+
+(deftest is-services-running
+  "Verifera att alla tjänster i systemet har startat och är igång"
+  (testing "gui-proxy"
+     (let [response (client/get "http://localhost:3000" )]
+       (is (= (:status response) 200))))
+  (testing "register-user"
+    (let [response (client/get "http://localhost:3001" )]
+      (is (= (:status response) 200)))))
+
+(defn -main
+[]
+(run-tests 'rent-a-thingy-req.main))

531da13e32db6e778287849fbf7d8ec70755956a
Jonathan M <Jonathanm87@gmail.com>
2015-12-09 19:43:37 +0100

    Nytt projekt register-tool

--- start-all.bat
+++ start-all.bat
@@ -7,8 +7,14 @@ REM register-user
 cd register-user
 start "register-user" lein ring server-headless
 cd .. 
- 
+
+REM user-info
 cd user-info
 start "user-info" lein ring server-headless
 cd ..  
+
+REM register-tool
+cd register-tool
+start "register-tool" lein ring server-headless
+cd .. 
  

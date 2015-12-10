ddcfc135fc19af9611c52f374ec4a313c422fe52
Roland Heimdahl <roland@javalia.se>
2015-12-10 20:01:29 +0100

    Ska man köra testerna?

--- gui-proxy/test/gui_proxy/handler_test.clj
+++ gui-proxy/test/gui_proxy/handler_test.clj
@@ -5,13 +5,11 @@
             [gui-proxy.db :as db]))
 
 (deftest test-app
-    (with-redefs [db/count-request (constantly 42)
-                  db/insert-request-info (constantly 0)]
-      (testing "home" (let [response (app (mock/request :get "/"))]
-                        (is (= (:status response) 200))
-                        (is (= (:body response) "Rent a thingy - gui-proxy 42 requests handled")))))
-
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404))))
-    )
+  (with-redefs [db/count-request (constantly 42)
+                db/insert-request-info (constantly 0)]
+    (testing "home" (let [response (app (mock/request :get "/"))]
+                      (is (= (:status response) 200))
+                      (is (= (:body response) "Rent a thingy - gui-proxy 42 requests handled"))))
+    (testing "not-found route"
+      (let [response (app (mock/request :get "/invalid"))]
+        (is (= (:status response) 404))))))

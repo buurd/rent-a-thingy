2821f4c5bfdb20f834f5e2352ba8f6dcd8fdb7a7
Roland Heimdahl <roland@javvalia.se>
2016-04-12 20:19:08 +0200

    felrättning av test

--- register-user/src/register_user/handler.clj
+++ register-user/src/register_user/handler.clj
@@ -3,20 +3,21 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.params :refer [wrap-params]]
+            [clojure.edn :as edn]
             [register-user.db :as db]))
 
 
 (defn validate-and-save! [body]
   (try (let [username (get body "username")]
          (db/insert-registration username (str body))
-         (str "register-user validate and save" body)
-         )
+         (str "register-user validate and save" body))
        (catch Exception e {:status 500 :body (str "SQL-fel insert " (.getMessage e))})))
 
 
 (defn validate-and-get-user [username]
   (try
-    (apply str (db/select-registration username))
+    (let [response (db/select-registration username)]
+      (apply merge (first response) (edn/read-string (:content (first response )))))
     (catch Exception e {:status 500 :body (str "SQL-fel select " (.getMessage e))})))
 
 (defroutes app-routes
--- register-user/test/register_user/handler_test.clj
+++ register-user/test/register_user/handler_test.clj
@@ -7,31 +7,32 @@
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Rent a thingy - register-user"))))
+      (is (= (:status response) 200) "main 1")
+      (is (= (:body response) "Rent a thingy - register-user") "main 2")))
 
   (testing "get registration"
-    (with-redefs [db/select-registration (constantly "kalle")]
-    (let [response (app (mock/request :get "/registration/kalle"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "kalle")))))
+    (with-redefs [db/select-registration (constantly '({:content "{:ort \"ort\" :username \"kalle\"}" :username "kalle"}))]
+      (let [response (app (mock/request :get "/registration/kalle"))]
+        (is (= (:status response) 200) "get registration 1")
+        (is (= (:username response) "kalle") "get registration 2"))))
 
   (testing "get registration throw exception"
     (with-redefs [db/select-registration (fn [] (throw (Exception.)))]
       (let [response (app (mock/request :get "/registration/kalle"))]
-        (is (= (:status response) 500))
-        (is (= (.startsWith(:body response) "SQL-fel"))))))
+        (is (= (:status response) 500) "get throw exception 1")
+        (is (= (.startsWith (:body response) "SQL-fel")) "get throw exception 2"))))
 
   (testing "register-user"
     (with-redefs [db/insert-registration (constantly {:username "kalle"})]
-      (let [response (app (mock/request :post "/register-user/" {:username "kalle"} ))]
-        (is (= (:status response) 200)))))
+      (let [response (app (mock/request :post "/register-user/" {:username "kalle"}))]
+        (is (= (:status response) 200) "register-user"))))
 
   (testing "register-user throw exeption"
     (with-redefs [db/insert-registration (fn [] (throw (Exception.)))]
-      (let [response (app (mock/request :post "/register-user/" {:username "kalle"} ))]
-        (is (= (:status response) 500)))))
+      (let [response (app (mock/request :post "/register-user/" {:username "kalle"}))]
+        (is (= (:status response) 500) "register-user throw exception"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404) "not-found-route"))))
+
